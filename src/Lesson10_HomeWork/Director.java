63b30862a2f39311acaf5514d58576bbbd6082eb
AnatolSalau <salovanatoly5@mail.ru>
2022-05-13 21:38:13 +0300

    Worked version 1,2,3

--- src/Lesson10_HomeWork/Director.java
+++ src/Lesson10_HomeWork/Director.java
@@ -6,7 +6,7 @@ class Director extends AbstractDirector{
         super(name, secondName, workExperience, position);
     }
 
-    public Director(String name, String secondName, double workExperience, Position position, AbstractEmployee[] abstractEmployees) {
+    public Director(String name, String secondName, double workExperience, Position position, AbstractEmployee... abstractEmployees) {
         super(name, secondName, workExperience, position, abstractEmployees);
     }
 

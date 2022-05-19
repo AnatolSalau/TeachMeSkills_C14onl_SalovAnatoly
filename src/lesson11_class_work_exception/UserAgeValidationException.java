b349b99651d5122ff70d764decf0b3cd1f281977
AnatolSalau <salovanatoly5@mail.ru>
2022-05-19 21:24:05 +0300

    Homework works  Lesson 12

--- src/lesson11_class_work_exception/UserAgeValidationException.java
+++
@@ -1,7 +0,0 @@
-package lesson11_class_work_exception;
-
-public class UserAgeValidationException extends UserValidationException{
-    public UserAgeValidationException(User user) {
-        super(user, "Users's age is not correct");
-    }
-}

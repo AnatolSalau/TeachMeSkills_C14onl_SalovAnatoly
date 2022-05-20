834a568deb66ccc792e98c5660478c0343113733
AnatolSalau <salovanatoly5@mail.ru>
2022-05-20 09:30:52 +0300

    Added ClassWork12 Files

--- src/lesson12_class_work/Main.java
+++ src/lesson12_class_work/Main.java
@@ -7,10 +7,7 @@ import java.io.IOException;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Lesson12");
-        try (FileWriter fileWriter = new FileWriter("src" + File.separator +
-                "lesson11_class_work_exception") {
-        {catch (IOException e) {
-            e.printStackTrace();
-        }
+
+    }
 }
+

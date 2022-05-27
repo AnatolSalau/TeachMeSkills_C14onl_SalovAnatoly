b64da0a242bf9b47e05971c7c5c7fd2129121a0f
AnatolSalau <salovanatoly5@mail.ru>
2022-05-27 11:09:22 +0300

    Lesson 14 Collections Generick ClassWork

--- src/lesson_14_generics_classwork/GenericAnExtend.java
+++ src/lesson_14_generics_classwork/GenericAnExtend.java
@@ -1,21 +1,29 @@
 package lesson_14_generics_classwork;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GenericAnExtend {
     public static void main(String[] args) {
         DBServiceExtend<Cl2,Cl4> serviceExtend = new DBServiceExtendImp<>();
         serviceExtend.save(new Cl3());
-
         //Коллекции с непонятным типом
-        //В лист можно добавить любого наследника Cl3
+        //В лист можно добавить любого наследника Cl3 паттерн PECS
         List<? super Cl3> list = new ArrayList<>();
         list.add(new Cl3());
         list.add(new Cl4());
         //Перебрать можем только через Object
         for (Object obj : list) {
             System.out.println(obj);
+        }
+        //Можно обойти данный паттерн используя метод .asList
+        List<? extends Cl3> list2 = Arrays.asList(new Cl3(),new Cl4());
+        //классический способ add не работает
+/*        list2.add(new Cl3());*/
+        //Можно обойти обычным foreach
+        for (Cl3 element : list2 ) {
+            System.out.println(element);
         }
     }
 }

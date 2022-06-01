aff9ed0141a6121a01acd61613d14db55a42d281
AnatolSalau <salovanatoly5@mail.ru>
2022-06-01 22:02:12 +0300

    ClassWork15 FunctionInterfaces

---
+++ src/lesson_16_classworks_streams/Member.java
@@ -0,0 +1,45 @@
+package lesson_16_classworks_streams;
+
+public class Member {
+    private String name;
+    private int age;
+    private SEX sex;
+    private boolean isAdult;
+
+    enum SEX {
+        MAN, WOMAN
+    }
+
+    public Member(String name, int age, SEX sex, boolean isAdult) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.isAdult = isAdult;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                ", isAdult=" + isAdult +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public SEX getSex() {
+        return sex;
+    }
+
+    public boolean isAdult() {
+        return isAdult;
+    }
+}
---
+++ src/lesson_16_classworks_streams/Stream01.java
@@ -0,0 +1,54 @@
+package lesson_16_classworks_streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Stream01 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("first", "second", "last", "fir", "fi");
+
+/*        //1 way
+        list.stream();
+        //2 way
+        int[] array = new int[] {0,1,2,3,4,5,6,7};
+        Arrays.stream(array);
+        //3 way
+        Stream.of("first", "second", "last");*/
+
+        //конвейерные методы (промежуточные)
+
+        //.filter - get Predicate from FunctionalInterface
+        Stream<String> myStream = list.stream().filter((element) -> element.length() >= 5);
+        //Stream is lazy
+        //Need to start stream
+        List<Integer> collect1 =
+                list.stream()
+                        //max size of stream
+                        .limit(4)
+                        //.distinct - отбросить повторяющиеся элементы
+                        .distinct()
+                        //сортируем с помощью Comaprable или Comparator
+/*                        .sorted((el1,el2) -> {return el1.length() > el2.length() ? -1;})*/
+                        .peek(System.out::println)
+                        .filter((element) -> element.startsWith("f"))
+                        .filter((element) -> element.length() >= 3)
+                        // .peek - get Concumer
+/*                        .peek(element -> System.out.println(element))*/
+                        //Metod reference
+                        .peek(System.out::println)
+                        //.map - get Function
+                        .map(text -> text.length())
+                        //Metod reference
+/*                        .map(String::length)*/
+                        //Integer stream
+                        //Terminate - запустить stream
+                        .collect(Collectors.toList());
+        System.out.println(collect1);
+        // .peek - get Concumer
+        //Стрим берет каждый элемент проводит его до конца проводя с ним все опрерации
+
+    }
+}

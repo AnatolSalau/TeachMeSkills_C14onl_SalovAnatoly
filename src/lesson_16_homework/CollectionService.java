9caeac283014b37330dccc0e5305c9b2f491b5a1
AnatolSalau <salovanatoly5@mail.ru>
2022-06-09 22:25:29 +0300

    Homework 16 Streams Task 1

--- src/lesson_16_homework/CollectionService.java
+++ src/lesson_16_homework/CollectionService.java
@@ -1,31 +1,28 @@
 package lesson_16_homework;
 
+import java.util.List;
+
 class CollectionService {
-    public enum EvenOrOdd {
-        EVEN, ODD
-    }
-    public Integer getIntegerWithConditions(Integer number, int startValue, int finishValue, EvenOrOdd evenOrOdd) {
-        Integer result = null;
-        switch (evenOrOdd) {
-            case EVEN -> {
+    public boolean getIntegerWithConditions(Integer number, int startValue, int finishValue) {
+        boolean result = false;
                 if (checkEvenOrNot(number) && number >= startValue && number <= finishValue) {
-                    result = number;
+                    result = true;
                 }
-            }
-            case ODD -> {
-                if (!checkEvenOrNot(number) && number >= startValue && number <= finishValue) {
-                    result = number;
-                }
-            }
-        }
         return result;
     }
-    private boolean checkEvenOrNot (Integer integer) {
+    public boolean checkEvenOrNot (Integer integer) {
         if (integer % 2 == 0) {
             return true;
         }
         else {
             return false;
         }
+    }
+    public int getAverage (List<Integer> integerList) {
+        int result = 0;
+        for (Integer num : integerList) {
+            result +=num;
+        }
+        return result/integerList.size();
     }
 }
---
+++ src/lesson_16_homework/Task2_Family.java
@@ -0,0 +1,8 @@
+package lesson_16_homework;
+
+class Task2_Family {
+    public static void main(String[] args) {
+/*        Child childOne = new Child("First",)*/
+
+    }
+}
